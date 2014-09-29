62b88aadb48f9780a24a22bf849cebc151a3c359
Steve Yi <steveyi32@gmail.com>
2014-09-29 15:05:50 -0500

    added slide show home page.

--- MyBusiness/MyBusiness/Models/Image.cs
+++ MyBusiness/MyBusiness/Models/Image.cs
@@ -9,14 +9,14 @@ namespace MyBusiness.Models
 {
     public class Image
     {
-        public Image(string path, string desc, string folder)
+        public Image(string path, string desc, string gender)
         {
             Path = path;
             Description = desc;
-            Folder = folder;
+            Gender = gender;
         }
         public string Path { get; set; }
         public string Description { get; set; }
-        public string Folder { get; set; }
+        public string Gender { get; set; }
     }
 }
